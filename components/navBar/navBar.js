003a999767742b638835849e5d2d984576fad3db
huowangW <2316802092@.com>
2020-07-27 15:29:22 +0800

    test

--- components/navBar/navBar.js
+++ components/navBar/navBar.js
@@ -1,66 +1,55 @@
-// pages/compoonents/navBar/navBar.js
-Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+Component({  
+  options: {  
+    multipleSlots: true // 在组件定义时的选项中启用多slot支持  
+  },  
+  /** 
+   * 组件的属性列表 
+   */  
+  properties: {  
+    toastText: {            // 属性名  
+      type: String,  
+      value: '/iconfont/shangyiyehoutuifanhui.svg'  
+    },
+    toastTex1: {
+      type:String,
+      value:"yonghu.svg"
+    },
+    title1:{
+      type:String,
+      value:"老司机同城"
+    },
+    title2:{
+      type:String,
+      value:"我的"
+    }
+  },  
+  /** 
+   * 组件的初始数据 
+   */  
+  data: {  
+    toastShow:false,  
+  },  
+  /** 
+   * 组件的方法列表 
+   */  
+  methods: {  
+    showToast(text,time) {  
+      this.setData({  
+        toastShow: !this.data.toastShow,  
+        toastText: text  
+      })  
+  
+      var that = this  
+  
+      if (!time){  
+        time = 8000  
+      }  
+  
+      setTimeout(function(){  
+        that.setData({  
+          toastShow: !that.data.toastShow  
+        })  
+      }, time)  
+    }  
+  }  
+})  
